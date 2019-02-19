99a885992887b0eec62971199d8e7457e117c3f3
J. Kobersky <kober32@gmail.com>
2019-02-19 18:55:47 +0100

    Added ping testing swift code

---
+++ Source/tests/iOS/PassMeterTester/PassMeterTester/PinGenerator.swift
@@ -0,0 +1,73 @@
+//
+// Copyright 2019 Wultra s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
+
+import Foundation
+import WultraPassMeter
+
+final class PinGenerator {
+    
+    private init() { }
+    
+    static func generate(url: URL) throws {
+        
+        FileManager.default.createFile(atPath: url.path, contents: nil, attributes: nil)
+        let h = try FileHandle(forWritingTo: url)
+        
+        var started = true
+        
+        pins(maxLength: testingPinLength) { pin, percent in
+            
+            //print(String(format: "%.2f%% evaluated \(pin)", percent*100))
+            
+            if pin.result.isEmpty == false {
+                h.write("\(started ? "" : ",")\(pin.fileValue)".data(using: .utf8)!)
+            }
+            
+            if started {
+                started = false
+            }
+        }
+    }
+    
+    static func pins(maxLength: Int, handle: (_ pin: Pin, _ percent: Double) -> Void) {
+        
+        guard maxLength > 3 else {
+            return
+        }
+        
+        // we only need to test 20% of the numbers to get the full coverage of variants (the rest is just variants for 1234....,2345....,3456....)
+        let capacity = Int(pow(10, Double(maxLength)))/5
+        
+        for pin in 0..<capacity {
+            let digits = numberOfDigits(in: pin)
+            for length in 4...maxLength {
+                if digits > length {
+                    continue
+                }
+                let formatted =  String(format: "%0\(length)d", pin)
+                handle(Pin(value: formatted, result: PasswordTester.shared.testPin(formatted)), Double(pin)/Double(capacity))
+            }
+        }
+    }
+    
+    private static func numberOfDigits(in number: Int) -> Int {
+        if number < 10 {
+            return 1
+        } else {
+            return 1 + numberOfDigits(in: number/10)
+        }
+    }
+}
---
+++ Source/tests/iOS/PassMeterTester/PassMeterTester/PinTester.swift
@@ -0,0 +1,71 @@
+//
+// Copyright 2019 Wultra s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
+
+import Foundation
+
+final class PinTester {
+    private init() { }
+    
+    static func test(_ url: URL) {
+        
+        guard var loadedPins = loadFromFile(url) else {
+            print("Failed to load pin file")
+            return
+        }
+        
+        var badPins = 0
+        
+        PinGenerator.pins(maxLength: testingPinLength) { pin, percent in
+            
+            if let filePin = loadedPins.removeValue(forKey: pin.value) {
+                if pin.result != filePin.result {
+                    print("\(pin.value) changed from \(filePin.result) to \(pin.result)")
+                    badPins += 1
+                }
+            } else if pin.result.isEmpty == false {
+                print("\(pin.value) changed from OK to \(pin.result)")
+                badPins += 1
+            }
+        }
+        
+        if badPins > 0 {
+            print("\(badPins) has changed from testing set.")
+        } else {
+            print("Everything looks 👌")
+        }
+    }
+    
+    private static func loadFromFile(_ url: URL) -> [String: Pin]? {
+        
+        let fileUrl = URL(fileURLWithPath: url.path)
+        
+        let data: Data
+        do {
+            data = try Data(contentsOf: fileUrl)
+        } catch let e {
+            print(e)
+            return nil
+        }
+        
+        guard let content = String(bytes: data, encoding: .utf8) else {
+            return nil
+        }
+        
+        let items = content.split(separator: ",").map { Pin(fromFileFormat: String($0)) }
+        
+        return Dictionary(uniqueKeysWithValues: items.map({ ($0.value, $0) }))
+    }
+}
