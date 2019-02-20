a7d883dbfe2861311a122719dc30db14bcc330b4
J. Kobersky <kober32@gmail.com>
2019-02-20 17:42:55 +0100

    added ios tests to test.sh

--- Source/tests/test.sh
+++ Source/tests/test.sh
@@ -1,22 +1,72 @@
 #!/bin/bash
 
-set -e # stop sript when error occures
+#set -e # stop sript when error occures
 #set -x # print all execution (good for debugging)
 
 testfile=$(pwd)/testfile.txt
 
-pushd "PassMeterTester"
-echo -e "\n\n\n\n\n UPDATING PODS \n\n\n\n\n"
-pod install
-echo -e "\n\n\n\n\n BUILDING TEST PROJECT \n\n\n\n\n"
-xcodebuild -quiet -workspace PassMeterTester.xcworkspace -configuration Debug -sdk macosx10.14 -scheme PassMeterTester build OBJROOT=$(PWD)/build SYMROOT=$(PWD)/build
+function section
+{
+    echo -e "\033[0;32m${1}\033[0m"
+}
 
-pushd "build/Debug"
+function success
+{
+    echo -e "\033[0;32m${1}\033[0m"
+}
 
-if [[ $1 = "generate" ]]; then
-    echo -e "\n\n\n\n\n RUNNING FILE GENERATING \n\n\n\n\n"
-    ./PassMeterTester "-generate" "${testfile}"
-else
-    echo -e "\n\n\n\n\n RUNNING TEST \n\n\n\n\n"
-    ./PassMeterTester "-test" "${testfile}"
-fi
+function error
+{
+    echo -e "\033[0;31m${1}\033[0m"
+}
+
+function qpopd
+{
+    popd > /dev/null
+}
+
+function qpushd 
+{
+    pushd "$1" > /dev/null
+}
+
+function consistencytest
+{
+    qpushd "PassMeterTester"
+    section "BUILDING CONSISTENCY TEST PROJECT"
+    xcodebuild -workspace PassMeterTester.xcworkspace -scheme PassMeterTester clean > /dev/null 2>&1
+    pod install > /dev/null
+    xcodebuild -quiet -workspace PassMeterTester.xcworkspace -configuration Debug -sdk macosx10.14 -scheme PassMeterTester build OBJROOT=$(PWD)/build SYMROOT=$(PWD)/build > /dev/null 2>&1
+
+    qpushd "build/Debug"
+
+    if [[ $1 = "generate" ]]; then
+        section "RUNNING FILE GENERATING"
+        ./PassMeterTester "-generate" "${testfile}"
+        exit 0
+    else
+        section "RUNNING CONSISTENCY TEST"
+        ./PassMeterTester "-test" "${testfile}"
+    fi
+
+    qpopd
+    qpopd
+}
+
+function iostest
+{
+    qpushd "../examples/iOS/PassMeterExample"
+    section "RUNNING iOS TESTS"
+    xcodebuild -workspace PassMeterExample.xcworkspace -scheme PassMeterExample clean > /dev/null 2>&1
+    pod install > /dev/null
+    xcodebuild -workspace PassMeterExample.xcworkspace -scheme PassMeterExample -destination 'platform=iOS Simulator,name=iPhone SE,OS=12.1' test > /dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        error "iOS TESTS FAILED"
+    else
+        success "iOS TESTS OK"
+    fi
+    qpopd
+}
+
+consistencytest
+iostest
